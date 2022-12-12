e923f36f801469504a708c598d7d12f07657f66f
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2022-12-12 15:27:44 +0700

    Role Permission

--- web-dashboard/app/Providers/RouteServiceProvider.php
+++ web-dashboard/app/Providers/RouteServiceProvider.php
@@ -17,7 +17,10 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/dashboard';
+    public const ADMIN = '/admin/dashboard';
+    public const STAFF = '/laundry/dashboard';
+    public const USER = '/user/dashboard';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
--- web-dashboard/database/migrations/2014_10_12_000000_create_users_table.php
+++ web-dashboard/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->integer('role_id')->unsigned();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('status');
             $table->rememberToken();
             $table->timestamps();
         });
