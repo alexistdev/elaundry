d19cbcdc933c21995155a15f76300d6b180c6c34
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2023-01-26 11:45:34 +0700

    Password Recovery Layout

--- Android-App/app/src/main/java/com/coder/elaundry_apps/ui/Login.java
+++ Android-App/app/src/main/java/com/coder/elaundry_apps/ui/Login.java
@@ -1,16 +1,26 @@
 package com.coder.elaundry_apps.ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 import com.coder.elaundry_apps.R;
 
 public class Login extends AppCompatActivity {
+    private TextView mPasswordRecovery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        this.initData();
+        mPasswordRecovery.setOnClickListener(v -> {
+            Intent intent = new Intent(Login.this, PasswordRecovery.class);
+            startActivity(intent);
+        });
+    }
+
+    public void initData(){
+        mPasswordRecovery = findViewById(R.id.txt_password_recovery);
     }
 }
