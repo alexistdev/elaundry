fda3b05be5bf62f0569acf012f6419d9e65bb62c
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2023-01-26 13:22:05 +0700

    Refactoring Menu Selected fragment

--- Android-App/app/src/main/java/com/coder/elaundry_apps/user/DashboardUser.java
+++ Android-App/app/src/main/java/com/coder/elaundry_apps/user/DashboardUser.java
@@ -1,22 +1,18 @@
 package com.coder.elaundry_apps.user;
 
-import androidx.annotation.NonNull;
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-
-import android.os.Bundle;
-import android.view.MenuItem;
-
 import com.coder.elaundry_apps.R;
 import com.coder.elaundry_apps.fragmentUser.HomeUser;
 import com.coder.elaundry_apps.fragmentUser.LaundryUser;
 import com.coder.elaundry_apps.fragmentUser.MessageUser;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 
 public class DashboardUser extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,18 +20,15 @@ public class DashboardUser extends AppCompatActivity {
         this.dataInit();
         bottomNavigationView.setOnItemSelectedListener(item -> {
             int id = item.getItemId();
-            Fragment fragment = null;
-            switch(id){
-                case R.id.home_menu:
-                    fragment = new HomeUser();
-                    break;
-                case R.id.laundry_menu:
-                    fragment = new LaundryUser();
-                    break;
-                case R.id.inbox_menu:
-                    fragment = new MessageUser();
-                    break;
+            Fragment fragment;
+            if(id == R.id.inbox_menu){
+                fragment = new MessageUser();
+            } else if(id == R.id.laundry_menu){
+                fragment = new LaundryUser();
+            } else {
+                fragment = new HomeUser();
             }
+
             return loadFragment(fragment);
         });
 
