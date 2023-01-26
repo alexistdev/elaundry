ddcb6adf290a554348d6ae1ca6ee25eeb64d154f
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2023-01-26 11:26:42 +0700

    Register Layout

---
+++ Android-App/app/src/main/java/com/coder/elaundry_apps/ui/Register.java
@@ -0,0 +1,16 @@
+package com.coder.elaundry_apps.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.coder.elaundry_apps.R;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
