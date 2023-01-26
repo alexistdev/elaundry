00cbdef3b03429305b8271e2d7c6c60ce58ae525
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2023-01-26 11:16:33 +0700

    Login layout

--- Android-App/app/src/main/java/com/coder/elaundry_apps/ui/Welcome.java
+++ Android-App/app/src/main/java/com/coder/elaundry_apps/ui/Welcome.java
@@ -2,9 +2,14 @@ package com.coder.elaundry_apps.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.coder.elaundry_apps.MainActivity;
 import com.coder.elaundry_apps.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Welcome extends AppCompatActivity {
 
@@ -12,5 +17,17 @@ public class Welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        int timeout = 3000;
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                finish();
+                Intent homepage = new Intent(Welcome.this, Login.class);
+                startActivity(homepage);
+            }
+        }, timeout);
     }
 }
